9d7a1fc07500fcda071a829aaa974c45ccb84271
hyunw3 <0176778602@naver.com>
2022-01-09 11:36:43 +0000

    5.3 Method Syntax : impl of struct, method, constructor

--- struct_exercise/src/main.rs
+++ struct_exercise/src/main.rs
@@ -4,18 +4,48 @@ struct Rectangle {
 	width : u32,
 	height : u32,
 }
+impl Rectangle {
+	fn square(size: u32) -> Rectangle { // associated function that isn't method : often used for constructors
+		Rectangle {
+			width : size,
+			height : size,
+		}
+	}
+	fn area(&self) -> u32 { // short for self: &self
+		self.width * self.height
+	}
+	fn width(&self) -> u32 { // method getter
+		self.width
+	}
+	fn available(&self) -> bool {
+		self.width >0 && self.height >0
+	}
+	fn can_hold(&self, other : &Rectangle) -> bool {
+		self.width > other.width && self.height > other.height
+	}
+}
 fn main() {
 	let scale = 2;
 	let rect1 = Rectangle{
 		width : dbg!(30 * scale),
-		height : 50
+		height : 40
 	};
+	if rect1.available() {
+		println!("The rectangle has a non-zero width & height; it is {} x {}",rect1.width,rect1.height)
+	}else {
+		return;
+	}
 	println!("rect1 is {:#?}",rect1);
 	println!(
 		"The area of rectangle is {} square pixels",
-		area(&rect1)
+		rect1.area()
 	);
-}
-fn area(dimension : &Rectangle) -> u32 {
-	dimension.width * dimension.height
+	
+	let rect2 = Rectangle {
+		width : 30,
+		height : 30
+	};
+	println!("Rect1 can hold ? : {}",rect1.can_hold(&rect2));
+	let sq = Rectangle::square(3);
+	println!("square constructor : {:#?}",sq);
 }
