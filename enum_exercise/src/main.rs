49a3b2d31d0fd943c78b3109bc811df42e216970
hyunw3 <0176778602@naver.com>
2022-01-11 14:43:05 +0000

    6.1 Enum fully usage with struct support

--- enum_exercise/src/main.rs
+++ enum_exercise/src/main.rs
@@ -1,26 +1,22 @@
 #[derive(Debug)]
-enum IpAddrKind {
-	V4,
-	V6
+struct Ipv4Addr {
+	ip : (u8, u8, u8, u8),
+	MAC : u32,
 }
-struct IpAddr {
-	kind : IpAddrKind,
-	address : String,
+#[derive(Debug)]
+enum IpAddr {
+	V4(Ipv4Addr),
+	V6(String),
 }
 fn main() {
-	let four = IpAddrKind::V4;
-	let six = IpAddrKind::V6;
 	// println!("four : {:?}, six : {:?}",four,six);
-	let home = IpAddr {
-		kind : IpAddrKind::V4,
-		address : String::from("127.0.0.1"),
-	};
-	let loopback = IpAddr {
-		kind : IpAddrKind::V6,
-		address : String::from("::1"),
-	};
+	let home = IpAddr::V4( Ipv4Addr {
+		ip :(127, 0, 0, 1),
+		MAC : 123134134,
+	});
 	
+	let loopback = IpAddr::V6(String::from("::1"));
+	// Option practice
+	println!("home : {:?}, loopback : {:?}",home,loopback);
 }
-fn route(ip_kind : IpAddrKind) {
-	
-}
+
