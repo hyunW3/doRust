d197644f6bc834d2daa34affd654195b41db977c
hyunw3 <0176778602@naver.com>
2022-01-11 14:58:21 +0000

    6.1 enum : absent_number(None Type) in Option has compile error

--- enum_exercise/src/main.rs
+++ enum_exercise/src/main.rs
@@ -8,15 +8,22 @@ enum IpAddr {
 	V4(Ipv4Addr),
 	V6(String),
 }
+enum Option<T> {
+	None,
+	Some(T),
+}
 fn main() {
-	// println!("four : {:?}, six : {:?}",four,six);
 	let home = IpAddr::V4( Ipv4Addr {
 		ip :(127, 0, 0, 1),
 		MAC : 123134134,
 	});
-	
 	let loopback = IpAddr::V6(String::from("::1"));
-	// Option practice
+	
 	println!("home : {:?}, loopback : {:?}",home,loopback);
+	// Option practice
+	let some_number = Some(5);
+	let some_string = Some("a string");
+	
+	let absent_number : Option<i32> = None;
 }
 
