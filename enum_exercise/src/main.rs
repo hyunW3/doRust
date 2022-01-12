5d90fc61f8bddcb02682162f21801507b44e45c9
hyunw3 <0176778602@naver.com>
2022-01-12 14:51:56 +0000

    6.2 match Control Flow Operator : Pstterns that Bind to Values

--- enum_exercise/src/main.rs
+++ enum_exercise/src/main.rs
@@ -12,6 +12,31 @@ enum Option<T> {
 	None,
 	Some(T),
 }
+#[derive(Debug)]
+enum UsState {
+	Alabama,
+	Alaska
+}
+enum Coin {
+	Penny,
+	Nickel,
+	Dime,
+	Quarter(UsState),
+}
+fn value_in_Cents(coin: Coin) -> u8 {
+	match coin {
+		Coin::Penny => {
+			println!("Lucky Penny!");
+			1
+		}
+		Coin::Nickel => 5,
+		Coin::Dime => 10,
+		Coin::Quarter(state) => {
+			println!("State quarter from {:?}",state);
+			25
+		},
+	}
+}
 fn main() {
 	let home = IpAddr::V4( Ipv4Addr {
 		ip :(127, 0, 0, 1),
@@ -20,10 +45,14 @@ fn main() {
 	let loopback = IpAddr::V6(String::from("::1"));
 	
 	println!("home : {:?}, loopback : {:?}",home,loopback);
-	// Option practice
+	// 6.1 Option practice
 	let some_number = Some(5);
 	let some_string = Some("a string");
 	
 	let absent_number : Option<i32> = None;
+	
+	// 6.2 match Control Flow
+	let quarter_alaska = Coin::Quarter(UsState::Alaska);
+	value_in_Cents(quarter_alaska);
 }
 
