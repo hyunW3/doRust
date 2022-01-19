eb00c9e5a6e17b5d05f880a2623be4d3c31a9df3
hyunw3 <0176778602@naver.com>
2022-01-19 12:02:59 +0000

    new challenge : curl system command apply but pipeline is not success

--- crawling/src/main.rs
+++ crawling/src/main.rs
@@ -1,19 +1,16 @@
-use std::io::{stdout, Write};
-use curl::easy::Easy;
-
+use std::process::Command;
+use std::io::{self, Write};
 // const NOTICE: &'static str = "https://skb.skku.edu/semi/community/notice.do";
 const NOTICE: &'static str = "https://www.skku.edu/skku/campus/skk_comm/notice01.do";
 fn get_link(url: &str) {
 }
+fn print_type_of<T>(_ : &T) {
+	println!("type is : {}",std::any::type_name::<T>())
+}
 fn main() {
 	// get_link(NOTICE);
-	let mut easy = Easy::new();
-	easy.url(NOTICE).unwrap();
-	easy.write_function(|data| {
-		stdout().write_all(data).unwrap();
-		Ok(data.len())
-	}).unwrap();
-	easy.perform().unwrap();
 	
-	println!("{}",easy.response_code().unwrap());
+	let output = Command::new("curl").args(&[NOTICE, "|", "tee","output.txt"]).output();
+	// io::stdout().write_all(&output.stdout).unwrap();
+	print_type_of(&output);
 }
