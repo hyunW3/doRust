af21dcd6ce21dc08de108891a190d8c05893ca91
hyunw3 <0176778602@naver.com>
2022-01-19 11:25:35 +0000

    curl OK but in skku notice, inpropriate approach is detected

--- crawling/src/main.rs
+++ crawling/src/main.rs
@@ -1,16 +1,19 @@
-extern crate reqwest;
-extern crate select;
+use std::io::{stdout, Write};
+use curl::easy::Easy;
 
-use select::document::Document;
-use select::predicate::Name;
-
-const NOTICE: &'static str = "https://skb.skku.edu/semi/community/notice.do";
-
+// const NOTICE: &'static str = "https://skb.skku.edu/semi/community/notice.do";
+const NOTICE: &'static str = "https://www.skku.edu/skku/campus/skk_comm/notice01.do";
+fn get_link(url: &str) {
+}
 fn main() {
-	let body = reqwest::get("https://skb.skku.edu/semi/community/notice.do")
-		.await?
-		.text()
-		.await?;
+	// get_link(NOTICE);
+	let mut easy = Easy::new();
+	easy.url(NOTICE).unwrap();
+	easy.write_function(|data| {
+		stdout().write_all(data).unwrap();
+		Ok(data.len())
+	}).unwrap();
+	easy.perform().unwrap();
 	
-	println!("body = {:?}",body);
+	println!("{}",easy.response_code().unwrap());
 }
