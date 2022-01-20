e129327e81e78a38cedc5aebaf694a989c076ecc
hyunw3 <0176778602@naver.com>
2022-01-20 15:02:25 +0000

    success crwaling html

--- crawling/src/main.rs
+++ crawling/src/main.rs
@@ -1,16 +1,14 @@
-use std::process::Command;
-use std::io::{self, Write};
 // const NOTICE: &'static str = "https://skb.skku.edu/semi/community/notice.do";
-const NOTICE: &'static str = "https://www.skku.edu/skku/campus/skk_comm/notice01.do";
-fn get_link(url: &str) {
+const NOTICE: &'static str = "https://rustrepo.com/repo/seanmonstar-reqwest-rust-http-client";
+
+use std::collections::HashMap;
+fn get_body() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::blocking::get(NOTICE)?
+        .text();
+		//.json::<HashMap<String, String>>()?;
+    println!("{:#?}", resp);
+    Ok(())
 }
-fn print_type_of<T>(_ : &T) {
-	println!("type is : {}",std::any::type_name::<T>())
-}
-fn main() {
-	// get_link(NOTICE);
-	
-	let output = Command::new("curl").args(&[NOTICE, "|", "tee","output.txt"]).output();
-	// io::stdout().write_all(&output.stdout).unwrap();
-	print_type_of(&output);
+fn main(){
+	get_body();
 }
